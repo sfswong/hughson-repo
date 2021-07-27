ee319f7c7c34ea2c7c791a03f791a29922e15e40
sfswong <88012320+sfswong@users.noreply.github.com>
2021-07-27 13:53:15 +0800

    Change title in the blog post

--- studio/src/structure/deskStructure.js
+++ studio/src/structure/deskStructure.js
@@ -58,7 +58,7 @@ export default () =>
         ),
       S.divider(),
       S.listItem()
-        .title('Blog posts')
+        .title('Blog posts1')
         .icon(MdDescription)
         .schemaType('post')
         .child(S.documentTypeList('post').title('Blog posts')),
